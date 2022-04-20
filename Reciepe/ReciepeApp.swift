7ff7fa32b71dfe1ac9b76bd0df77f7e66e2c7cf3
mmold4 <moldochm@hotmail.com>
2022-04-20 18:51:32 -0400

    Initial Commit

---
+++ Reciepe/ReciepeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciepeApp.swift
+//  Reciepe
+//
+//  Created by Michael Moldoch on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ReciepeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
